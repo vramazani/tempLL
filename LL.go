965c8d1109a1b17b72655aaca79981c17083764f
Vahid <18081068+vramazani@users.noreply.github.com>
2020-04-30 01:52:39 +0430

    clean up

--- LL.go
+++ LL.go
@@ -20,49 +20,38 @@ type LinkedList struct {
 
 func (list *LinkedList) append(node *Node) int {
 	// consider both empty and non-empty list
-	if list.len == 0 {
+	if 0 == list.len {
 		list.first = node
-		list.last = node
-		list.len = 1
 	} else {
 		list.last.next = node
 		node.prev = list.last
-		list.last = node
-		list.len++
 	}
+	list.last = node
+	list.len++
 	return list.len
 }
 
 func (list *LinkedList) pop() (ret int, e error) {
-	switch list.len {
-	case 0:
+	if 0 == list.len {
 		return 0, errors.New("empty")
-	case 1:
-		ret = list.first.data
+	}
+	ret = list.last.data
+	if 1 == list.len {
 		list.first = nil
 		list.last = nil
-		list.len = 0
-		return
-	case 2:
-		ret = list.last.data
-		list.first.next = nil
-		list.last = list.first
-		list.len = 1
-		return
-	default:
-		ret = list.last.data
+	} else {
 		list.last = list.last.prev
 		list.last.next = nil
-		list.len--
-		return
 	}
+	list.len--
+	return
 }
 
 func (list *LinkedList) find(data, count int) (output []*Node) {
-	if list.len == 0 || count < 1 {
+	// def: finds [first] up to count occurances of data in list
+	if 0 == list.len || 0 == count {
 		return
 	} else {
-		// output := []*Node {}
 		current := list.first
 		for i := 0; i < list.len; i++ {
 			if current.data == data {
@@ -77,27 +66,33 @@ func (list *LinkedList) find(data, count int) (output []*Node) {
 }
 
 func (list *LinkedList) remove(data, count int) (countDone int) {
+	// def: removes [first] up to count occurances of data in list
 	nodes := list.find(data, count)
-	// list.len : 0, 1, 2, 3+
-	// 0: nodes is an empty array
-	// 1:
+
+	// list.len in the for : 0, 1, 2, 3+
+	// 0: only when initially empty; skips for cuz LL.find yielded an empty array
+	// 1: first&&last
+	// 2: first / last
+	// 3+: first / last / inner
 	for i := range nodes {
 		if nodes[i] == list.first && nodes[i] == list.last {
 			// here list.len == 1
+			// hence must be the last iteration
 			list.first = nil
 			list.last = nil
 			list.len = 0
 			countDone++
+			// here should countDone == len(nodes) == i+1
 			return
 		} else if nodes[i] == list.first {
-			// here list.len == 2
+			// here list.len >= 2
 			list.first = list.first.next
 			list.first.prev = nil
 		} else if nodes[i] == list.last {
-			// here list.len == 2
+			// here list.len >= 2
 			list.last = list.last.prev
 			list.last.next = nil
-		} else {
+		} else { // inner
 			// here list.len >= 3
 			nodes[i].prev.next = nodes[i].next
 			nodes[i].next.prev = nodes[i].prev
